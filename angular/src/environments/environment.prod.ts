d5fa7db07e0b7e71d9ba126f8a0f6c8840a23cbc
Shivam Agrahari <shivam.aracdgame@gmail.com>
2025-07-21 00:07:08 +0530

    Update environment.prod.ts

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  SERVER: 'https://your-production-backend-url.com/api/',
+  SERVERURL: 'https://your-production-backend-url.com/',
+  RECORDRURL: 'https://your-production-backend-url.com/recordings/'
 };
